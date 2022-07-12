59ee627d01db9253a918def1f1231e8f92e9edf7
metalkutz <metalkutz@gmail.com>
2022-07-12 22:48:50 +0200

    codigo PCA y mod reglog, update main y NLP

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from nltk.stem.snowball import SnowballStemmer
 import nltk
 from sklearn.feature_extraction.text import  TfidfVectorizer, TfidfTransformer, CountVectorizer
 
+###### PCA #######
+from sklearn.decomposition import PCA
+
 ##########  libreria para entrenamiento #############
 
 
@@ -92,3 +95,17 @@ np.set_printoptions(precision=2)
 vectorizador = TfidfVectorizer()
 matriz_palabras = vectorizador.fit_transform(descripcion)
 matriz_palabras = matriz_palabras.astype('float32') # cambiamos el tipo a float32 para disminuir uso de memoria
+
+############ PCA ##############
+df2 = pd.DataFrame(matriz_palabras.toarray())  # el array de matriz palabras pasamos a dataframe
+df2.columns = vectorizador.get_feature_names_out() # agregamos nombres a las columnas con las palabras del vocabulario
+pca2 = PCA(n_components=2000) # objeto de PCA con un máximo de 2000 componentes
+pca2 = pca2.fit(df2) # ajustamos el PCA al df2 de matriz de palabras
+lista_PCA = [ 'PC'+str(i) for i in range(len(pca2.components_)) ] # generamos la lista de nombres de componentes del PCA
+reduced_data = pca2.transform(df2)  # aplicamos la transformación al dataframe de la matriz de palabras reduciendo la dimensionalidad
+reduced_data = pd.DataFrame(reduced_data, columns = lista_PCA) # agregamos nombre de las columnas asociadas a los componentes del PCA
+temp = df.reset_index() #df original reseteamos el indice para poder concatenar
+df3 = pd.concat([temp,reduced_data], axis=1) #concatenamos dataframe original con componentes
+df3.drop(['Descripcion raiz limpia'], axis=1, inplace=True) # eliminamos columna de descripcion
+
+############ datos para train y test ###########
