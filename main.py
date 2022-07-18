ee7418d5481509ebca6db2996ea6d3924bd6bc00
metalkutz <metalkutz@gmail.com>
2022-07-18 16:17:54 +0200

    update main con referencias a archivos pickles

--- main.py
+++ main.py
@@ -1,81 +1,68 @@
+# %%
+####### funciones, carga de datos inicial
+from funciones import* 
 
+####### os, pickle
+import os
+import pickle
+
+###### pandas, numpy, funciones
 import pandas as pd
 import numpy as np
-from funciones import *
-#### librerias para NLP ##########
-import re
-import regex
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-import nltk
-from sklearn.feature_extraction.text import  TfidfVectorizer, TfidfTransformer, CountVectorizer
-
-###### PCA #######
-from sklearn.decomposition import PCA
-
-######### re-balanceo de muestras ##########
-from imblearn.over_sampling import SMOTE 
 
 ##########  libreria para entrenamiento #############
 
 
-
+# %%
 ######### Carga, preprocesamiento datos ###################
-'carga inicial dataset desde archivo csv a un objeto panda'
-data = pd.read_csv(r'.\Datos\dataset_mercado_publico.csv', delimiter=';')
-df = data.copy() # hacemos una copia sobre la cual trabajar
-df = df.rename(columns={'Tender_id':'id_licitacion','Item_Key':'id_producto','Nombre linea Adquisicion':'nombre_producto','Descripcion linea Adquisicion':'descripcion','Kupfer':'label'})
-df.index = df['id_producto']  # cambiamos el indice del dataframe por el id_producto
-
-'se eliminan columnas que no se utilizaran'
-df.drop(columns=['id_licitacion','id_producto','Rubro2','Rubro3','nombre_producto'], inplace=True)
-
-'limpieza de datos, nans, duplicados'
-df.dropna(axis=0, inplace = True) #Si alguna fila tiene un NaN se elimina la fila 
-df.drop_duplicates(keep='first', inplace=True) # elimina los registros duplicados
-
-'creación de variables'
-df = pd.get_dummies(df, columns=['Rubro1'] ,drop_first=True) # convertimos var categorica Rubro1 en dummy
-
-
-########### NLP #################
-df['Descripcion limpia']= df['descripcion'].astype(str) #Convertimos la columna en string para poder trabajar con el texto
-
-
-#Aplicamos la función texto_limpio para limpiar las descripciones
-df['Descripcion limpia'] = df['Descripcion limpia'].apply(lambda texto: texto_limpio(texto)) 
-
-
-# vamos a trabajar con las palabras estemizadas/raices
-df['descripcion']= df['descripcion'].astype(str)
-df['Descripcion raiz limpia']= df['descripcion'].apply(lambda texto: texto_raiz(texto)) #Aplicamos la función texto_raiz que nos convierte las palabras en sus raíces y las limpia
-df.drop(columns=['descripcion','Descripcion limpia'], axis=1, inplace=True)
-
-
-# ahora vectorizamos 
-descripcion = np.array(df['Descripcion raiz limpia']) # array para armar el bag of words
-np.set_printoptions(precision=2)
+os.chdir(r'Datos')
+# %%
+##### archivo generado con el codigo dataset_train_test.py
+fh1=open('data.pkl','rb')
+data=pickle.load(fh1)
+fh1.close()
+data.head()
+# %%
+########### NLP ################# 
+##### archivo generado con el codigo dataset_train_test.py
+fh2=open('df_nltk.pkl','rb')
+df=pickle.load(fh2)
+fh2.close()
+df.head()
+# %%
+# ahora vectorizamos y creamos el bag of words
 
 # forma corta TFIDF vectorizer
-vectorizador = TfidfVectorizer()
-matriz_palabras = vectorizador.fit_transform(descripcion)
-matriz_palabras = matriz_palabras.astype('float32') # cambiamos el tipo a float32 para disminuir uso de memoria
-# %%
+##### archivo generado con el codigo dataset_train_test.py
+'''fh3 = open('df_tfidf.pkl','wb')
+pickle.dump(df2,fh3)
+fh3.close()
+df2.head()'''
 ############ PCA ##############
-df2 = pd.DataFrame(matriz_palabras.toarray())  # el array de matriz palabras pasamos a dataframe
-df2.columns = vectorizador.get_feature_names() # agregamos nombres a las columnas con las palabras del vocabulario
-pca = PCA(n_components=2000) # objeto de PCA con un máximo de 2000 componentes
-pca = pca.fit(df2) # ajustamos el PCA al df2 de matriz de palabras
-lista_PCA = [ 'PC'+str(i) for i in range(len(pca.components_)) ] # generamos la lista de nombres de componentes del PCA
-reduced_data = pca.transform(df2)  # aplicamos la transformación al dataframe de la matriz de palabras reduciendo la dimensionalidad
-reduced_data = pd.DataFrame(reduced_data, columns = lista_PCA) # agregamos nombre de las columnas asociadas a los componentes del PCA
-temp = df.reset_index() #df original reseteamos el indice para poder concatenar
-df3 = pd.concat([temp,reduced_data], axis=1) #concatenamos dataframe original con componentes
-df3.drop(['Descripcion raiz limpia'], axis=1, inplace=True) # eliminamos columna de descripcion
-
+##### archivo generado con el codigo dataset_train_test.py
+'''fh4=open('df_PCA100.pkl.pkl','rb')
+dff3=pickle.load(fh4)
+fh4.close()
+df3.head()'''
 ############ datos para train y test ###########
-X = df3.drop(columns=['id_producto','label'], axis=1) # creamos la variables independientes
-y = df3['label']  # creamos la variable dependiente
+##### archivo generado con el codigo dataset_train_test.py
+fh5=open('df_smote30_train_test.pkl','rb')
+train_test=pickle.load(fh5)
+fh5.close()
 
-oversampling = SMOTE(sampling_strategy=0.30) # usamos oversampling sintético podemos elegir el nivel de oversampling con  sampling_strategy=0.80
-X_balanceado, y_balanceado = oversampling.fit_resample(X, y) #Se obtienen nuevos X e y
+
+X_balanceado = train_test['X_balanceado']
+y_balanceado = train_test['y_balanceado']
+print('X:',X_balanceado.shape,'y:',y_balanceado.shape)
+
+# %%
+##### archivo reducido en componentes PCA = 100 para prueba de flujos
+fh6=open('df_smote30_train_test(redux).pkl','rb')
+train_test_redux=pickle.load(fh6)
+fh6.close()
+
+
+X_balanceado_redux = train_test_redux['X_balanceado']
+y_balanceado_redux = train_test_redux['y_balanceado']
+print('X:',X_balanceado_redux.shape,'y:',y_balanceado_redux.shape)
+# %%
