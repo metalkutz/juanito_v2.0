9dc5a9d80fd3794d84f7f4eab69792df8ffe8497
metalkutz <metalkutz@gmail.com>
2022-07-19 10:30:43 +0200

    update dataset_train_test

--- dataset_train_test.py
+++ dataset_train_test.py
@@ -19,13 +19,20 @@ from sklearn.decomposition import PCA
 ######### re-balanceo de muestras ##########
 from imblearn.over_sampling import SMOTE 
 # %%
-df.head()
+'cambiamos a directorio de datos para guardar los dataframes creados'
+os.chdir(r'Datos')
+
 # %%
 ####### desde archivo Carga_dataset se trae dataframe con
 'carga inicial dataset desde archivo csv a un objeto panda'
 'se eliminan columnas que no se utilizaran'
 'limpieza de datos, nans, duplicados'
 'creación de variables'
+
+#guardamos el datafra con la data original
+fh = open('data.pkl','wb')
+pickle.dump(data,fh)
+fh.close()
 # %%
 ########### NLP #################
 df['Descripcion limpia']= df['descripcion'].astype(str) #Convertimos la columna en string para poder trabajar con el texto
@@ -38,6 +45,10 @@ df['descripcion']= df['descripcion'].astype(str)
 df['Descripcion raiz limpia']= df['descripcion'].apply(lambda texto: texto_raiz(texto)) #Aplicamos la función texto_raiz que nos convierte las palabras en sus raíces y las limpia
 df.drop(columns=['descripcion','Descripcion limpia'], axis=1, inplace=True)
 
+#guardamos el dataframe aplicando de nltk para limpieza de campo descripcion
+fh = open('df_nltk.pkl','wb')
+pickle.dump(df,fh)
+fh.close()
 # %%
 'bag of words (vector de palabras) y escalamos con TF-IDF'
 # ahora vectorizamos 
@@ -53,6 +64,11 @@ matriz_palabras = matriz_palabras.astype('float32') # cambiamos el tipo a float3
 df2 = pd.DataFrame(matriz_palabras.toarray())  # el array de matriz palabras pasamos a dataframe
 df2.columns = vectorizador.get_feature_names() # agregamos nombres a las columnas con las palabras del vocabulario
 # %%
+'''# guardamos el dataframe con la bolsa de palabras escalado con TF-IDF
+fh = open('df_tfidf.pkl','wb')
+pickle.dump(df2,fh)
+fh.close()'''
+# %%
 pca = PCA(n_components=100) # objeto de PCA con un máximo de 2000 componentes
 pca = pca.fit(df2) # ajustamos el PCA al df2 de matriz de palabras
 lista_PCA = [ 'PC'+str(i) for i in range(len(pca.components_)) ] # generamos la lista de nombres de componentes del PCA
@@ -61,6 +77,16 @@ reduced_data = pd.DataFrame(reduced_data, columns = lista_PCA) # agregamos nombr
 temp = df.reset_index() #df original reseteamos el indice para poder concatenar
 df3 = pd.concat([temp,reduced_data], axis=1) #concatenamos dataframe original con componentes
 df3.drop(['Descripcion raiz limpia'], axis=1, inplace=True) # eliminamos columna de descripcion
+# %%
+'''# guardamos el dataframe con los componentes principales PCA
+fh = open('df_PCA100.pkl','wb')
+pickle.dump(reduced_data,fh)
+fh.close()'''
+
+'''# guardamos el dataframe final previo a sobremuestreo SMOTE
+fh = open('df_union.pkl','wb')
+pickle.dump(df3,fh)
+fh.close()'''
 
 # %%
 ############ datos para train y test ###########
@@ -70,9 +96,7 @@ y = df3['label']  # creamos la variable dependiente
 oversampling = SMOTE(sampling_strategy=0.30) # usamos oversampling sintético podemos elegir el nivel de oversampling con  sampling_strategy=0.80
 X_balanceado, y_balanceado = oversampling.fit_resample(X, y) #Se obtienen nuevos X e y
 # %%
-'cambiamos a directorio de datos para guardar los dataframes creados'
-os.chdir(r'Datos')
-# %%
+############# ARCHIVOS MUY PESADOS para guardar en GIT o que no se usan 
 # guardamos el dataframe final para entrenamiento aplicando sobremuestreo SMOTE con sobremuestreo de 30%
 train_test = {'X_balanceado':X_balanceado,'y_balanceado':y_balanceado}
 fh = open('df_smote30_train_test.pkl','wb')
@@ -80,30 +104,3 @@ fh = open('df_smote30_train_test.pkl','wb')
 pickle.dump(train_test,fh)
 fh.close()
 ####### OJO QUE ARCHIVO PESA 2GB
-# %%
-############# ARCHIVOS MUY PESADOS para guardar en GIT o que no se usan 
-# %%
-#guardamos el datafra con la data original
-fh = open('data.pkl','wb')
-pickle.dump(data,fh)
-fh.close()
-# %%
-#guardamos el dataframe aplicando de nltk para limpieza de campo descripcion
-fh = open('df_nltk.pkl','wb')
-pickle.dump(df,fh)
-fh.close()
-# %%
-# guardamos el dataframe con la bolsa de palabras escalado con TF-IDF
-fh = open('df_tfidf.pkl','wb')
-pickle.dump(df2,fh)
-fh.close()
-# %%
-# guardamos el dataframe con los componentes principales PCA
-fh = open('df_PCA100.pkl','wb')
-pickle.dump(df3,fh)
-fh.close()
-# %%
-# guardamos el dataframe final previo a sobremuestreo SMOTE
-fh = open('df_union.pkl','wb')
-pickle.dump(df3,fh)
-fh.close()
