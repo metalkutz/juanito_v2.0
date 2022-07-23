27739a2644d75692395a6aca34df1bd095af6d2a
metalkutz <metalkutz@gmail.com>
2022-07-23 13:40:49 +0200

    update main

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ import numpy as np
 
 # %%
 ######### Carga, preprocesamiento datos ###################
-os.chdir(r'Datos')
+#os.chdir(r'Datos')
 # %%
 ##### archivo generado con el codigo dataset_train_test.py
-fh1=open('data.pkl','rb')
+fh1=open('.\Datos\data.pkl','rb')
 data=pickle.load(fh1)
 fh1.close()
 data.head()
 # %%
 ##### archivo generado con el codigo dataset_train_test.py
-fh0=open('df_categorias.pkl','rb')
+fh0=open('.\Datos\df_categorias.pkl','rb')
 train_test_cat=pickle.load(fh0)
 fh0.close()
 
@@ -31,22 +31,16 @@ print('X_cat:',X_cat.shape,'y_cat:',y_cat.shape)
 # %%
 ########### NLP ################# 
 ##### archivo generado con el codigo dataset_train_test.py
-fh2=open('df_nltk.pkl','rb')
+fh2=open('.\Datos\df_nltk.pkl','rb')
 df=pickle.load(fh2)
 fh2.close()
 df.head()
 # %%
 # ahora vectorizamos y creamos el bag of words
 
-# forma corta TFIDF vectorizer
-##### archivo generado con el codigo dataset_train_test.py
-'''fh3 = open('df_tfidf.pkl','rb')
-df_tfidf=pickle.load(df_tfidf,fh3)
-fh3.close()
-df_tfidf.head()'''
 ############ PCA ##############
 ##### archivo generado con el codigo dataset_train_test.py
-fh4=open('df_PCA10k.pkl','rb')
+fh4=open('.\Datos\df_PCA10k.pkl','rb')
 reduced_data=pickle.load(fh4)
 fh4.close()
 reduced_data.head()
@@ -54,7 +48,7 @@ reduced_data.head()
 # %%
 ############ datos para train y test ###########
 ##### archivo generado con el codigo dataset_train_test.py
-fh5=open('df_PCA10k_train_test.pkl','rb')
+fh5=open('.\Datos\df_PCA10k_train_test.pkl','rb')
 train_test=pickle.load(fh5)
 fh5.close()
 
